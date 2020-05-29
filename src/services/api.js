3424d2ac0e6cfb44b206f475ab6cf046fd175d25
Leonardo <leodaiub@gmail.com>
2020-05-29 13:05:56 -0300

    netlify deploy fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import store from "store";
 
-const api = axios.create({ baseURL: process.env.API_URL });
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
 api.interceptors.request.use((config) => {
   if (!!store.get("token"))
